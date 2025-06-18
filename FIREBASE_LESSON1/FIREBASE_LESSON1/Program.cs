be369c9c1330f01aad7034f507aa446cc2d2fae3
Quách Thành Long <stephensouth1307@gmail.com>
2025-06-18 10:02:03 +0700

    -UPDATE FIREBASE PACKAGES
    Gitgnore file Author

--- FIREBASE_LESSON1/FIREBASE_LESSON1/Program.cs
+++ FIREBASE_LESSON1/FIREBASE_LESSON1/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using Firebase.Database;
 namespace FIREBASE_LESSON1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Khởi tạo Firebase...");
+
+            FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromFile("D:\\VTC Academy\\Game Programing Language\\K24GD03_PROGRAMING_CS\\FIREBASE_LESSON1\\FIREBASE_LESSON1\\bin\\Debug\\private_key.json")
+            });
+
+            Console.WriteLine("Firebase Admin SDK đã được khởi tạo thành công !");
         }
     }
 }
