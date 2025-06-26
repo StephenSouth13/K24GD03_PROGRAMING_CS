3738805d18737fa856ac74d31c3cbf9d0591bbc1
Quách Thành Long <stephensouth1307@gmail.com>
2025-06-26 10:31:34 +0700

    - Update Lab7

--- Lab2/Lab2/obj/Debug/net9.0/Lab2.AssemblyInfo.cs
+++ Lab2/Lab2/obj/Debug/net9.0/Lab2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+277e3a90deefe1c05dd8b330407967168765d636")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bb2d8f04f7f0c14ab52f3d241f0ea9f39355fe3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
